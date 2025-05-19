8c3f33346c57f7b0aac40c34974bf7ed3ab2b724
ChadMetz <chadmetz@me.com>
2025-05-18 20:35:44 -0500

    Add files via upload

---
+++ man/spc_help.R
@@ -0,0 +1,18 @@
+#' @title SPC Chart Functions
+#' @description This document outlines the help references for SPC chart functions in the qiTools package.
+#' 
+#' @section Chart Types:
+#' 
+#' - \code{xbar_chart()}: X-Bar Chart for monitoring process mean.
+#' - \code{r_chart()}: R Chart for monitoring subgroup range.
+#' - \code{c_chart()}: C Chart for counts of events per sample.
+#' - \code{imr_chart()}: Individuals and Moving Range charts for individual measurements.
+#' 
+#' Each chart function returns a \code{ggplot2} object for visualization.
+#'
+#' @seealso \link[qiTools]{xbar_chart}, \link[qiTools]{r_chart}, \link[qiTools]{c_chart}, \link[qiTools]{imr_chart}
+#'
+#' @name spc_help
+#' @rdname spc_help
+#' @keywords documentation
+NULL
